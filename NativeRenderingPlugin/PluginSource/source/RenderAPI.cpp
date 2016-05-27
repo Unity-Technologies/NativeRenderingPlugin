9772b1b70001a88c38b0217418357cd8dd38b713
Aras Pranckevičius <348087+aras-p@users.noreply.github.com>
2016-05-27 11:25:04 +0300

    NativeRenderingPlugin: fix clang warnings

--- NativeRenderingPlugin/PluginSource/source/RenderAPI.cpp
+++ NativeRenderingPlugin/PluginSource/source/RenderAPI.cpp
@@ -8,7 +8,7 @@ RenderAPI* CreateRenderAPI(UnityGfxRenderer apiType)
 #	if SUPPORT_D3D11
 	if (apiType == kUnityGfxRendererD3D11)
 	{
-		RenderAPI* CreateRenderAPI_D3D11();
+		extern RenderAPI* CreateRenderAPI_D3D11();
 		return CreateRenderAPI_D3D11();
 	}
 #	endif // if SUPPORT_D3D11
@@ -16,7 +16,7 @@ RenderAPI* CreateRenderAPI(UnityGfxRenderer apiType)
 #	if SUPPORT_D3D9
 	if (apiType == kUnityGfxRendererD3D9)
 	{
-		RenderAPI* CreateRenderAPI_D3D9();
+		extern RenderAPI* CreateRenderAPI_D3D9();
 		return CreateRenderAPI_D3D9();
 	}
 #	endif // if SUPPORT_D3D9
@@ -24,7 +24,7 @@ RenderAPI* CreateRenderAPI(UnityGfxRenderer apiType)
 #	if SUPPORT_D3D12
 	if (apiType == kUnityGfxRendererD3D12)
 	{
-		RenderAPI* CreateRenderAPI_D3D12();
+		extern RenderAPI* CreateRenderAPI_D3D12();
 		return CreateRenderAPI_D3D12();
 	}
 #	endif // if SUPPORT_D3D9
@@ -33,7 +33,7 @@ RenderAPI* CreateRenderAPI(UnityGfxRenderer apiType)
 #	if SUPPORT_OPENGL_UNIFIED
 	if (apiType == kUnityGfxRendererOpenGLCore || apiType == kUnityGfxRendererOpenGLES20 || apiType == kUnityGfxRendererOpenGLES30)
 	{
-		RenderAPI* CreateRenderAPI_OpenGLCoreES(UnityGfxRenderer apiType);
+		extern RenderAPI* CreateRenderAPI_OpenGLCoreES(UnityGfxRenderer apiType);
 		return CreateRenderAPI_OpenGLCoreES(apiType);
 	}
 #	endif // if SUPPORT_OPENGL_UNIFIED
@@ -41,7 +41,7 @@ RenderAPI* CreateRenderAPI(UnityGfxRenderer apiType)
 #	if SUPPORT_OPENGL_LEGACY
 	if (apiType == kUnityGfxRendererOpenGL)
 	{
-		RenderAPI* CreateRenderAPI_OpenGL2();
+		extern RenderAPI* CreateRenderAPI_OpenGL2();
 		return CreateRenderAPI_OpenGL2();
 	}
 #	endif // if SUPPORT_OPENGL_LEGACY
