f4eaa6d4c78980b386f80cb4c2d60a2299886269
Aras Pranckevičius <348087+aras-p@users.noreply.github.com>
2016-05-30 10:32:47 +0300

    [bufferptr] Use the new plugin function to modify native vertex buffer

--- NativeRenderingPlugin/UnityProject/Assets/UseRenderingPlugin.cs
+++ NativeRenderingPlugin/UnityProject/Assets/UseRenderingPlugin.cs
@@ -29,6 +29,15 @@ public class UseRenderingPlugin : MonoBehaviour
 #endif
 	private static extern void SetTextureFromUnity(System.IntPtr texture, int w, int h);
 
+	// We'll pass native pointer to the mesh vertex buffer.
+	// Also passing source unmodified mesh data.
+	// The plugin will fill vertex data from native code.
+#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+#else
+	[DllImport ("RenderingPlugin")]
+#endif
+	private static extern void SetMeshBuffersFromUnity (IntPtr vertexBuffer, int vertexCount, IntPtr sourceVertices, IntPtr sourceNormals, IntPtr sourceUVs);
 
 #if UNITY_IPHONE && !UNITY_EDITOR
 	[DllImport ("__Internal")]
@@ -41,6 +50,7 @@ public class UseRenderingPlugin : MonoBehaviour
 	IEnumerator Start()
 	{
 		CreateTextureAndPassToPlugin();
+		SendMeshBuffersToPlugin();
 		yield return StartCoroutine("CallPluginAtEndOfFrames");
 	}
 
@@ -59,6 +69,35 @@ public class UseRenderingPlugin : MonoBehaviour
 		// Pass texture pointer to the plugin
 		SetTextureFromUnity (tex.GetNativeTexturePtr(), tex.width, tex.height);
 	}
+
+	private void SendMeshBuffersToPlugin ()
+	{
+		var filter = GetComponent<MeshFilter> ();
+		var mesh = filter.mesh;
+		// The plugin will want to modify the vertex buffer -- on many platforms
+		// for that to work we have to mark mesh as "dynamic" (which makes the buffers CPU writable --
+		// by default they are immutable and only GPU-readable).
+		mesh.MarkDynamic ();
+
+		// However, mesh being dynamic also means that the CPU on most platforms can not
+		// read from the vertex buffer. Our plugin also wants original mesh data,
+		// so let's pass it as pointers to regular C# arrays.
+		// This bit shows how to pass array pointers to native plugins without doing an expensive
+		// copy: you have to get a GCHandle, and get raw address of that.
+		var vertices = mesh.vertices;
+		var normals = mesh.normals;
+		var uvs = mesh.uv;
+		GCHandle gcVertices = GCHandle.Alloc (vertices, GCHandleType.Pinned);
+		GCHandle gcNormals = GCHandle.Alloc (normals, GCHandleType.Pinned);
+		GCHandle gcUV = GCHandle.Alloc (uvs, GCHandleType.Pinned);
+
+		SetMeshBuffersFromUnity (mesh.GetNativeVertexBufferPtr (0), mesh.vertexCount, gcVertices.AddrOfPinnedObject (), gcNormals.AddrOfPinnedObject (), gcUV.AddrOfPinnedObject ());
+
+		gcVertices.Free ();
+		gcNormals.Free ();
+		gcUV.Free ();
+	}
+
 
 	private IEnumerator CallPluginAtEndOfFrames()
 	{
