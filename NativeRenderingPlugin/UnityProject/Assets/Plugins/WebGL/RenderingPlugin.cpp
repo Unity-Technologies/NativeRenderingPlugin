b0981ec41b72bb59035aa29a654cd9d311bd68f8
Marco Trivellato <marcot@unity3d.com>
2016-11-16 09:50:55 +0000

    bulk-build rendering plugin source on webgl and update readme file

---
+++ NativeRenderingPlugin/UnityProject/Assets/Plugins/WebGL/RenderingPlugin.cpp
@@ -0,0 +1,4 @@
+
+#include "../../../../PluginSource/Source/RenderingPlugin.cpp"
+#include "../../../../PluginSource/Source/RenderAPI.cpp"
+#include "../../../../PluginSource/Source/RenderAPI_OpenGLCoreES.cpp"
