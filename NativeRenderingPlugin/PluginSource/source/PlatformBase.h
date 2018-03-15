fc9073ea8e79921ca3539a8a94a12fc716612915
Alexey Orlov <alexey@unity3d.com>
2018-03-15 14:25:19 +0300

    removed d3d9 support from plugin itself

--- NativeRenderingPlugin/PluginSource/source/PlatformBase.h
+++ NativeRenderingPlugin/PluginSource/source/PlatformBase.h
@@ -39,7 +39,6 @@
 		#define SUPPORT_D3D12 1
 	#endif
 #elif UNITY_WIN
-	#define SUPPORT_D3D9 1
 	#define SUPPORT_D3D11 1 // comment this out if you don't have D3D11 header/library files
 	#define SUPPORT_D3D12 0 //@TODO: enable by default? comment this out if you don't have D3D12 header/library files
 	#define SUPPORT_OPENGL_UNIFIED 1
