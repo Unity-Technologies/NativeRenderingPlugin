28681df525c1cd12a6d7aa45409bd3c0ee0a224f
Xiao Lei <xiao.lei@unity3d.com>
2022-05-26 17:41:21 -0700

    Fix typo in IUnityGraphicsVulkan

--- PluginSource/source/Unity/IUnityGraphicsVulkan.h
+++ PluginSource/source/Unity/IUnityGraphicsVulkan.h
@@ -93,14 +93,14 @@ enum UnityVulkanEventRenderPassPreCondition
     // This is the default precondition
     kUnityVulkanRenderPass_DontCare,
 
-    // Make sure that there is currently no RenderPass in progress.
-    // This allows e.g. resource uploads.
+    // Make sure that there is currently RenderPass in progress.
     // There are no guarantees about the currently bound descriptor sets, vertex buffers, index buffers and pipeline objects
     // Unity does however set dynamic pipeline set VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR based on the current settings
     // If used in combination with the SRP RenderPass API the resuls is undefined
     kUnityVulkanRenderPass_EnsureInside,
 
     // Make sure that there is currently no RenderPass in progress.
+	// This allows e.g. resource uploads.
     // Ends the current render pass (and resumes it afterwards if needed)
     // If used in combination with the SRP RenderPass API the resuls is undefined.
     kUnityVulkanRenderPass_EnsureOutside
